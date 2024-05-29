5eccb95ba0066bf0310c0662ee3cab762f219f41
Simon Ståhl <simon.stahl94@gmail.com>
2024-05-29 11:01:59 +0200

    delat upp i klasser

---
+++ Labb6 - XUnit&Acceptanstestning/Calculation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Labb6___XUnit_Acceptanstestning.Calculator;
+
+namespace Labb6___XUnit_Acceptanstestning
+{
+    public class Calculation
+    {
+        public double Number1 { get; }
+        public double Number2 { get; }
+        public Operator Operator { get; }
+        public double Result { get; }
+
+        public Calculation(double num1, double num2, Operator op)
+        {
+            Number1 = num1;
+            Number2 = num2;
+            Operator = op;
+            Result = PerformCalculation();
+        }
+
+        private double PerformCalculation()
+        {
+            switch (Operator)
+            {
+                case Operator.Addition:
+                    return Number1 + Number2;
+                case Operator.Subtraction:
+                    return Number1 - Number2;
+                case Operator.Multiplication:
+                    return Number1 * Number2;
+                case Operator.Division:
+                    if (Number2 != 0)
+                        return Number1 / Number2;
+                    else
+                    {
+                        Console.WriteLine("Det går inte att dividera med noll.");
+                        return double.NaN;
+                    }
+                default:
+                    throw new InvalidOperationException("Ogiltig operator.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Number1} {OperatorToString()} {Number2} = {Result}";
+        }
+
+        private string OperatorToString()
+        {
+            switch (Operator)
+            {
+                case Operator.Addition:
+                    return "+";
+                case Operator.Subtraction:
+                    return "-";
+                case Operator.Multiplication:
+                    return "*";
+                case Operator.Division:
+                    return "/";
+                default:
+                    throw new InvalidOperationException("Ogiltig operator.");
+            }
+        }
+    }
+}
---
+++ Labb6 - XUnit&Acceptanstestning/Calculator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb6___XUnit_Acceptanstestning
+{
+    public class Calculator
+    {
+        private List<Calculation> calculations = new List<Calculation>();
+        public void Start()
+        {
+            bool continueCalculating = true;
+
+            while (continueCalculating)
+            {
+                Console.WriteLine("Välj ett räknesätt:");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraktion");
+                Console.WriteLine("3. Multiplikation");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Visa tidigare beräkningar");
+                Console.WriteLine("6. Avsluta");
+
+                int choice = GetUserChoice();
+
+                switch (choice)
+                {
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        PerformCalculation((Operator)(choice - 1));
+                        break;
+                    case 5:
+                        ShowPreviousCalculations();
+                        break;
+                    case 6:
+                        continueCalculating = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val. Försök igen.");
+                        break;
+                }
+            }
+        }
+
+        private int GetUserChoice()
+        {
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 6)
+            {
+                Console.WriteLine("Ogiltigt val. Försök igen.");
+            }
+            return choice;
+        }
+
+        private void PerformCalculation(Operator operation)
+        {
+            double num1 = GetUserNumber("Ange det första talet: ");
+            double num2 = GetUserNumber("Ange det andra talet: ");
+
+            Calculation calculation = new Calculation(num1, num2, operation);
+            calculations.Add(calculation);
+
+            double result = calculation.Result;
+            Console.WriteLine($"Resultatet är: {result}");
+        }
+
+        private void ShowPreviousCalculations()
+        {
+            Console.WriteLine("Tidigare beräkningar:");
+            foreach (var calculation in calculations)
+            {
+                Console.WriteLine(calculation);
+            }
+        }
+
+        private double GetUserNumber(string message)
+        {
+            double num;
+            Console.Write(message);
+            while (!double.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Ogiltig inmatning. Försök igen.");
+                Console.Write(message);
+            }
+            return num;
+        }
+
+        public enum Operator
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
+    }
+}
+
